d5e6fe056afc9b8ade9f80cc99120ac533c56140
hmmi <hmmi@kakao.com>
2023-12-14 16:15:30 +0900

    study: Component Basic

--- blog/src/App.js
+++ blog/src/App.js
@@ -44,8 +44,19 @@ function App() {
         } }> 정렬버튼 </button>
         <hr/>
       </div>      
+      <Modal/>
     </div>
   );
+}
+
+function Modal(){
+  return(
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
